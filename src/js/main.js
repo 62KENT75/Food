7a027aa005765efa1c73728960721a06530f937a
Sergey_Gershun <gershun62@gmail.com>
2024-02-15 12:41:53 +0200

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,30 @@
+import tabs from './modules/tabs';
+import modal from './modules/modal';
+import timer from './modules/timer';
+import cards from './modules/cards';
+import calculator from './modules/calculator';
+import forms from './modules/forms';
+import sliders from './modules/sliders';
+import { openModal } from './modules/modal';
+
 window.addEventListener('DOMContentLoaded', () => {
-    const tabs = require('./modules/tabs'),
-          modal = require('./modules/modal'),
-          timer = require('./modules/timer'),
-          cards = require('./modules/cards'),
-          calculator = require('./modules/calculator'),
-          forms = require('./modules/forms'),
-          sliders = require('./modules/sliders');
+    const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 50000);
     
-    tabs();
-    modal();
-    timer();
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+    modal('[data-modal]', '.modal', modalTimerId);
+    timer('.timer', '2024-03-03');
     cards();
     calculator();
-    forms();
-    sliders();
+    forms('form', modalTimerId);
+    sliders({
+        container: '.offer__slider',
+        slide: ".offer__slide",
+        nextArrow: ".offer__slider-next",
+        prevArrow: ".offer__slider-prev",
+        totalCounter: "#total",
+        currentCounter: "#current",
+        wrapper: ".offer__slider-wrapper",
+        field: ".offer__slider-inner"
+    });
     
 });
--- src/js/modules/timer.js
+++ src/js/modules/timer.js
@@ -1,6 +1,4 @@
-function timer() {
-    // Timer
-    const deadline = '2024-1-30';
+function timer(id, deadline) {
 
     function getTimeRemaining(endtime) {
         let days, hours, minutes, seconds;
@@ -59,7 +57,7 @@ function timer() {
         }
     }
 
-    setClock('.timer', deadline);
+    setClock(id, deadline);
 }
 
-module.exports = timer;
+export default timer;
